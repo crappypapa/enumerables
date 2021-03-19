47564e3fb18426c3ce158cc5d046137cec250d6d
Zil Norvilis <zilasino27@gmail.com>
2021-03-19 10:19:12 +0100

    Add my_all method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -17,19 +17,33 @@ def my_each_with_index
   end
   self
 end
-#["a","b","c","d"].my_each_with_index{|item,index| p "#{item} ,#{index}"}
-def my_select
-  arr=[]
-  self.my_each do |item|
-    if yield(item)
-      arr << item
+# ["a","b","c","d"].my_each_with_index{|item,index| p "#{item} ,#{index}"}
+
+  def my_select
+    arr=[]
+    self.my_each do |item|
+      if yield(item)
+        arr << item
+      end
+    end
+    arr
+  end
+# p [1,2,3,4,5].my_select { |num|  num % 2 == 0 }
+# p [:foo, :bar].my_select { |x| x == :foo } 
+
+def my_all?(param=nil)
+  return true if self.length == 0    #return true if empty array given
+
+  if param
+    if param.is_a?(Regexp)
+      return self.my_select { |el| el =~ param }.length == self.to_a.length
+    elsif param.is_a?(Class)
+      return self.my_select { |el| el.is_a?(param) }.length == self.to_a.length
     end
   end
-  arr
 end
-# p [1,2,3,4,5].my_select { |num|  num % 2 == 0 }
-p [:foo, :bar].my_select { |x| x == :foo } 
-
+# p %w[ant bear cat].my_all?(/t/)
+p [1, 2i, 3.14].my_all?(Numeric)
 
 
 end
