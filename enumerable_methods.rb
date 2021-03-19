d0dfdcb2a43c02aa8ce88165ed9dc23623ba8cb2
Zil Norvilis <zilasino27@gmail.com>
2021-03-19 10:56:32 +0100

    Finish my_any method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -50,11 +50,48 @@ def my_all?(param=nil)
     class_type = self[0].class
     return self.my_select { |el| el.class == class_type }.length == self.to_a.length
   end
+
 end
 # p %w[ant bear cat].my_all?(/t/)
 # p [1, 2i, 3.14].my_all?(Numeric)
 # p %w[ant bear cat].all? { |word| word.length >= 3 }
 # p [nil, true, 99].all?
 
+def my_any?(param=nil)
+  if param  # If there is parameter given
+    if param.is_a?(Regexp)  # check if Regex class passed as 'parameter'
+      self.my_each do |el|
+        if (el =~ param)  # if at least one regex is matched
+          return true  # return true
+        end
+      end
+    elsif param.is_a?(Class)
+      self.my_each do |el|
+        if (el.is_a?(param))  # if at least one element class is matched with Class given via param 
+          return true # return true
+        end
+      end
+    end
+    return false # if my_each ends without finding given class in any el return false
+  end
+
+  if block_given?
+    self.my_each do |el|
+      if yield(el)
+        return true
+      end
+    end
+    return false
+  end
+
+  if !param && !block_given?
+    self.my_each { |el| return true if (el != nil || el != false) }
+  end
+
+end
+# p %w[ant bear cat].my_any?(/d/)
+# p [nil, true, 99].my_any?(Integer)
+# p %w[ant bear cat].my_any? { |word| word.length >= 4 }
+p [nil, true, false].any?
 
 end
