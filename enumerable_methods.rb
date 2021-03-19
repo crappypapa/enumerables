e170fa62bd82815a1e1b6d82affeb0f292ef0934
crappypapa <devsolaoluwa@gmail.com>
2021-03-19 09:54:05 +0100

    Add my_each_with_index

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -8,4 +8,21 @@ def my_each
   self
 end
 # [1,3,5,7].my_each{|number| p number}
+
+def my_each_with_index
+  index=0
+  self.my_each do |item|
+    yield(item, index)
+    index +=1
+  end
+  self
+end
+  ["a","b","c","d"].my_each_with_index{|item,index| p "#{item} ,#{index}"}
+
+
+
+
+
+
+
 end
